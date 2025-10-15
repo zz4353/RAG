b6120e4b1e7ead44e85867f1a74281cf14f90962
chirikatori <thanhnd9904@gmail.com>
2025-10-15 08:11:44 +0700

    Update

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from app.llm.chat import ask_llm, ask_rag
 from app.vector_rag.indexer import load_and_index_data
 from app.vector_rag.collections import COLLECTIONS
 
-question = "Xin chào, bạn là mô hình gì?"
-answer = ask_llm(question)
-print(answer)
-print("--------------------------------------------------------")
 
-question = "Con vịt có mấy chân?"
-docs = ["Con vịt có 4 chân."]
-answer = ask_rag(question, docs)
-print(answer)
-print("--------------------------------------------------------")
+if __name__ == "__main__":
+    question = "Xin chào, bạn là mô hình gì?"
+    answer = ask_llm(question)
+    print(answer)
+    print("--------------------------------------------------------")
 
-load_and_index_data(COLLECTIONS["books"], "data/books")  # index data xong thì comment lại.
+    question = "Con vịt có mấy chân?"
+    docs = ["Con vịt có 4 chân."]
+    answer = ask_rag(question, docs)
+    print(answer)
+    print("--------------------------------------------------------")
 
-question = "phụ nữ muốn gì ở đàn ông?"
-docs = COLLECTIONS["books"].search(question, top_k=3, threshold=0.3)
-print(docs)
-print("--------------------------------------------------------")
+    load_and_index_data(COLLECTIONS["books"], "data/books")  # index data xong thì comment lại.
 
-answer = ask_rag(question, docs)
-print(answer)
+    question = "phụ nữ muốn gì ở đàn ông?"
+    docs = COLLECTIONS["books"].search(question, top_k=3, threshold=0.3)
+    print(docs)
+    print("--------------------------------------------------------")
+
+    answer = ask_rag(question, docs)
+    print(answer)
